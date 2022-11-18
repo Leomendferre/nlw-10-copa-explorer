8f41e9eda08cb7eb7e15bf01162708484704cdb0
Leonardo Mendonça Ferreira <leo.mend@hotmail.com>
2022-11-17 21:38:03 -0300

    Adicionando jogos da frança

--- main.js
+++ main.js
@@ -22,17 +22,20 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =   
-    createCard("22/11", "terça", createGame("argentina", "13:00", "saudi-arabia")) + 
+    createCard("22/11", "terça", createGame("argentina", "07:00", "saudi-arabia") + 
+    createGame("france", "16:00", "australia")) +
 
     createCard("24/11", "quinta", createGame("portugal", "13:00", "ghana") +
       createGame("brazil", "16:00", "serbia")) +
 
-    createCard("26/11", "sábado", createGame("argentina", "13:00", "mexico")) +  
+    createCard("26/11", "sábado", createGame("france", "13:00", "denmark") +  
+      createGame("argentina", "16:00", "mexico")) +
 
     createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland") + 
       createGame("portugal", "16:00", "uruguay")) +
 
-    createCard("30/11", "quarta", createGame("poland", "13:00", "argentina")) +  
+    createCard("30/11", "quarta", createGame("tunisia", "12:00", "france") +  
+      createGame("poland", "16:00", "argentina")) +
 
-    createCard("02/12", "sexta", createGame("portugal", "16:00", "south-korea") +
+    createCard("02/12", "sexta", createGame("portugal", "12:00", "south-korea") +
       createGame("cameroon", "16:00", "brazil"))
