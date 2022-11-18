994f70cc4714af3133c722e1977fc0f46b044cb6
Leonardo Mendonça Ferreira <leo.mend@hotmail.com>
2022-11-17 21:21:32 -0300

    Inserindo jogos do meu interesse.

--- main.js
+++ main.js
@@ -22,8 +22,17 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =   
-    createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+    createCard("22/11", "terça", createGame("argentina", "13:00", "saudi-arabia")) + 
+
+    createCard("24/11", "quinta", createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")) +
+
+    createCard("26/11", "sábado", createGame("argentina", "13:00", "mexico")) +  
+
     createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland") + 
       createGame("portugal", "16:00", "uruguay")) +
-    createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
 
+    createCard("30/11", "quarta", createGame("poland", "13:00", "argentina")) +  
+
+    createCard("02/12", "sexta", createGame("portugal", "16:00", "south-korea") +
+      createGame("cameroon", "16:00", "brazil"))
